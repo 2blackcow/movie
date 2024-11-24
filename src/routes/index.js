1be7921e4faa5aae536f24445953cf98d7387d5c
2blackcow <138091561+2blackcow@users.noreply.github.com>
2024-11-24 13:09:21 +0900

    라우터 경로 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,48 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/Home"
+import Home from "../pages/Home";
 import SearchPage from "../pages/SearchPage";
 import MyList from "../pages/MyList";
-import Browse from "../pages/Browse"; 
+import Browse from "../pages/Browse";
 import Popular from "../pages/Popular";
 import MovieDetail from "../pages/MovieDetail";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
     {
-        path : "/",
-        element : <App/>,
-        children : [
-            {
-                path : "",
-                element : <Home/>
-            },
-            {
-                path: 'movie',
-                element: <Home/>
-            },
-            {
-                path: 'popular',
-                element: <Popular />
-            },
-            {
-                path: 'wishlist',
-                element: <MyList />
-            },
-            {
-                path: 'browse',
-                element: <Browse/>
-            },
-            {
-                path : "movie/:id",
-                element : <MovieDetail/>
-            },
-            {
-                path : "search",
-                element : <SearchPage/>
-            }
-        ]
-        
-    }
-]);
+      path: "/",
+      element: <App />,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "movie", element: <Home /> },
+        { path: "popular", element: <Popular /> },
+        { path: "wishlist", element: <MyList /> },
+        { path: "browse", element: <Browse /> },
+        { path: "movie/:id", element: <MovieDetail /> },
+        { path: "search", element: <SearchPage /> },
+      ],
+    },
+  ],
+  { basename: "/movie/" }
+);
 
 export default router;
